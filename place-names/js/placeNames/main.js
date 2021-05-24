349885b3f1999c8c784526253a235badabbab39b
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-24 11:24:32 -0500

    Completed place names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,24 @@
-// Put your code here
+const names = [
+    "Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+]
+
+console.log("All Place Names");
+
+names.forEach(name => {
+    console.log(name);
+})
+
+console.log("")
+
+const theNames = names.filter(name => {
+
+    if (!name.search("The")) {
+        return name;
+    }
+});
+
+console.log("'The' Place Names");
+theNames.forEach(name => {
+    console.log(name)
+})
